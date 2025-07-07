fb494ae898c86e7a184acd3e2c2319ae2c7bd981
sergei00026 <sergei57fx@mail.ru>
2025-07-07 23:39:57 +0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {}
+};
